c81a7a633a6ccdb1f8ccd898d97a0ae1a2c3b5e8
HelenMallett <[helenkwmallett@googlemail.com]>
2016-06-30 15:45:45 +0100

    initial state of the system for testing

---
+++ mod_tests.py
@@ -0,0 +1,9 @@
+# This is for you to fill in!
+
+from mean import *
+
+def test_mean_12345():
+    num_list=[1,2,3,4,5]
+    calc_mean=mean(num_list)
+    expected_mean=3
+    assert expected_mean==calc_mean
